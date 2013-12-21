1b099f3db5aeed275df2b27f1cd3c2e224dad827
Cliff Crosland <cliffcrosland@gmail.com>
2013-12-21 13:39:59 -0800

    IT'S ALIVE!!!

--- index.js
+++ index.js
@@ -9,29 +9,37 @@ var firstName = 'Clifton';
 var lastName = 'Crosland';
 
 var submitCheckInForm = function () {
-  console.log("Submitting check in form");
-  driver.findElement(webdriver.By.id('check-in-panel')).click();
-  driver.findElement(webdriver.By.id('checkin_confnum')).sendKeys(confirmationNumber);
-  driver.findElement(webdriver.By.id('checkin_firstname')).sendKeys(firstName);
-  driver.findElement(webdriver.By.id('checkin_lastname')).sendKeys(lastName);
-  driver.findElement(webdriver.By.id('checkin_confnum')).submit();
+  console.log("Submitting check in form...");
+  var checkInPanel = driver.findElement(webdriver.By.id('check-in-panel'));
+  var confnum = driver.findElement(webdriver.By.id('checkin_confnum'));
+  var firstname = driver.findElement(webdriver.By.id('checkin_firstname'));
+  var lastname = driver.findElement(webdriver.By.id('checkin_lastname'));
+  checkInPanel.click();
+  confnum.clear();
+  confnum.sendKeys(confirmationNumber);
+  firstname.clear();
+  firstname.sendKeys(firstName);
+  lastname.clear();
+  lastname.sendKeys(lastName);
+  confnum.submit();
   driver.sleep(5000);
 };
 
 var confirmCheckIn = function () {
-  console.log("Confirming Check In");
+  console.log("Confirming check in...");
   driver.findElement(webdriver.By.id('printDocumentsButton')).click();
   driver.sleep(5000);
 };
 
 var sendBoardingPassUrl = function () {
-  console.log("Sending boarding pass URL");
+  console.log("Sending boarding pass URL...");
   driver.getCurrentUrl().then(function (url) {
     console.log("URL OF YOUR BOARDING DOCUMENT: ");
     console.log(url);
   })
 };
 
+console.log("Visiting southwest.com...");
 driver.get("http://www.southwest.com").
   then(submitCheckInForm).
   then(confirmCheckIn).
