183bf701e4d003f4db683f374cd3da453378492e
Cliff Crosland <cliffcrosland@gmail.com>
2013-12-21 13:25:57 -0800

    Add console.log to log the running of each step.

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var firstName = 'Clifton';
 var lastName = 'Crosland';
 
 var submitCheckInForm = function () {
+  console.log("Submitting check in form");
   driver.findElement(webdriver.By.id('check-in-panel')).click();
   driver.findElement(webdriver.By.id('checkin_confnum')).sendKeys(confirmationNumber);
   driver.findElement(webdriver.By.id('checkin_firstname')).sendKeys(firstName);
@@ -18,11 +19,13 @@ var submitCheckInForm = function () {
 };
 
 var confirmCheckIn = function () {
+  console.log("Confirming Check In");
   driver.findElement(webdriver.By.id('printDocumentsButton')).click();
   driver.sleep(5000);
 };
 
-var sendPrintBoardingPassUrl = function () {
+var sendBoardingPassUrl = function () {
+  console.log("Sending boarding pass URL");
   driver.getCurrentUrl().then(function (url) {
     console.log("URL OF YOUR BOARDING DOCUMENT: ");
     console.log(url);
@@ -32,7 +35,7 @@ var sendPrintBoardingPassUrl = function () {
 driver.get("http://www.southwest.com").
   then(submitCheckInForm).
   then(confirmCheckIn).
-  then(sendPrintBoardingPassUrl).
+  then(sendBoardingPassUrl).
   then(null, function (err) {
     console.log("An error occurred!");
     console.log(err);
